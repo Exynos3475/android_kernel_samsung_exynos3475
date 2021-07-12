06e1cb4a5727ffaf72f6e31d5401926be2c0b4bd
Seden Ecem <ttbpccorporation@outlook.com>
2021-07-12 10:24:44 +0300

    usb: gadget: Remove redefined entry

--- include/linux/usb/gadget.h
+++ include/linux/usb/gadget.h
@@ -575,22 +575,6 @@ static inline size_t usb_ep_align(struct usb_ep *ep, size_t len)
 
 /**
  * usb_ep_align_maybe - returns @len aligned to ep's maxpacketsize if gadget
- *	requires quirk_ep_out_aligned_size, otherwise returns len.
- * @g: controller to check for quirk
- * @ep: the endpoint whose maxpacketsize is used to align @len
- * @len: buffer size's length to align to @ep's maxpacketsize
- *
- * This helper is used in case it's required for any reason to check and maybe
- * align buffer's size to an ep's maxpacketsize.
- */
-static inline size_t
-usb_ep_align_maybe(struct usb_gadget *g, struct usb_ep *ep, size_t len)
-{
-	return g->quirk_ep_out_aligned_size ? usb_ep_align(ep, len) : len;
-}
-
-/**
- * usb_ep_align_maybe - returns @len aligned to ep's maxpacketsize if gadget
  *	requires quirk_ep_out_aligned_size, otherwise reguens len.
  * @g: controller to check for quirk
  * @ep: the endpoint whose maxpacketsize is used to align @len
